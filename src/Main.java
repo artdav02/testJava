4eed85d15afb0be8305455ab3df508a691c56f83
artdav02 <artdav2706@gmail.com>
2023-05-27 02:20:44 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        class Node {
+            private String name;
+            private Node firstChild;
+            private Node nextSibling;
+
+            Node(String n, Node d, Node r) {
+                name = n;
+                firstChild = d;
+                nextSibling = r;
+            }
+        }
+
     }
 }
